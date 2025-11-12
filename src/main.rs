70200d523e58b122ab6e3aaf285d65e9268982ea
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-11-11 22:31:39 -0500

    initial test of mclaren website load time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use time::OffsetDateTime;
+
 fn main() {
-    println!("Hello, world!");
+    let mclaren_load_time = get_load_time("https://www.mclaren.com");
+    println!("McLaren Load Time: {}", mclaren_load_time);
+}
+
+fn get_load_time(website_url: &str) -> f64 {
+    let start = OffsetDateTime::now_utc();
+    let _ = reqwest::blocking::get(website_url);
+    let end = OffsetDateTime::now_utc();
+    let duration = end - start;
+    return duration.as_seconds_f64();
 }
