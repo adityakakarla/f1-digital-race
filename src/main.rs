5c9fd06efb71a3e7e51b672412f4e4da55c3c8a8
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-11-11 22:57:52 -0500

    added driver sites + multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
+use std::{
+    sync::{Arc, Barrier, Mutex, atomic::AtomicI8},
+    thread::{self, JoinHandle},
+};
 use time::OffsetDateTime;
 
 fn main() {
-    let mclaren_load_time = get_load_time("https://www.mclaren.com");
-    println!("McLaren Load Time: {}", mclaren_load_time);
+    let urls = vec![
+        "https://www.landonorris.com",
+        "https://www.oscarpiastri.com",
+        "https://www.verstappen.com",
+        "https://www.georgerussell63.com",
+        "https://charlesleclerc.com/en/",
+        "https://www.lewishamilton.com",
+        "https://www.mercedesamgf1.com/drivers/driver/andrea-kimi-antonelli",
+        "https://www.alexalbon.com",
+        "https://www.nicohulkenberg.net",
+        "https://www.isackhadjar.com",
+        "https://www.olliebearman.com",
+        "https://www.fernandoalonso.com",
+        "https://www.carlossainz.es/en/",
+        "https://www.liamlawson30.com",
+        "https://www.lancestroll.com",
+        "https://www.esteban-ocon.com",
+        "https://www.yukitsunoda.com",
+        "https://www.pierregasly.com/en/",
+        "https://www.gabrielbortoleto.com.br/en/",
+        "https://www.francolapinto.com",
+    ];
+    let mut handles: Vec<JoinHandle<()>> = vec![];
+    let barrier = Arc::new(Barrier::new(urls.len()));
+    let counter = Arc::new(AtomicI8::new(1));
+
+    for url in urls {
+        let barrier = barrier.clone();
+        let counter = counter.clone();
+        let handle = thread::spawn(move || {
+            barrier.wait();
+            let time = get_load_time(url);
+            let rank = counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+            println!("P{:?}: {} ({})", rank, url, time);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
 
 fn get_load_time(website_url: &str) -> f64 {
